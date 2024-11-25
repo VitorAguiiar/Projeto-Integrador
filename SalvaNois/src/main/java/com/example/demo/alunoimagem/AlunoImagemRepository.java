d2084031f3fe15264c7d59344a3005b6f35d18f2
Igula07 <nicolaspereirasenai@gmail.com>
2024-11-25 15:54:53 -0300

    Delete SalvaNois directory

--- SalvaNois/src/main/java/com/example/demo/alunoimagem/AlunoImagemRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.alunoimagem;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlunoImagemRepository extends JpaRepository<AlunoImagem, Long> {
-}
