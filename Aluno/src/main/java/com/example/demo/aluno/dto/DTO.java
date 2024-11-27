e413620cb21626e08721af2e76a0b652a760186d
Igula07 <nicolaspereirasenai@gmail.com>
2024-11-26 21:04:40 -0300

    Delete Aluno directory

--- Aluno/src/main/java/com/example/demo/aluno/dto/DTO.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.aluno.dto;
-
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-public record DTO(String user, String telefone, String email, String imagemUrl) {
-}
